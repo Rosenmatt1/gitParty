21a0a04371f5aadae7b4c59f3a82b3165a45dd8f
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-01-17 17:15:13 -0700

    Add an express http server

---
+++ serverExpress.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8000;
+
+app.disable('x-powered-by');
+
+app.use(function (req, res) {
+  var guests = ['Mary', 'Don'];
+  res.send(guests);
+});
+
+app.listen(port, function () {
+  console.log('Listening on port', port);
+});
