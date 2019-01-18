f52da0f4906abfae415f1013a99fb3263b0f8c86
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-01-17 17:40:18 -0700

    refactored code

--- serverExpress.js
+++ serverExpress.js
@@ -1,15 +1,46 @@
+var fs = require('fs');
+var path = require('path');
+var guestsPath = path.join(__dirname, 'guests.json');
+
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
 
 app.disable('x-powered-by');
 
-app.get('/guests', (req, res) => {
-  var guests = ['Mary', 'Don'];
-  res.send(guests);
+app.get('/guests', function (req, res) {
+  fs.readFile(guestsPath, 'utf8', function (err, guestsJSON) {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    var guests = JSON.parse(guestsJSON);
+
+    res.send(guests);
+  });
 });
 
-app.use((req, res) => {
+app.get('/guests/:id', function (req, res) {
+  fs.readFile(guestsPath, 'utf8', function (err, guestsJSON) {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    var id = Number.parseInt(req.params.id);
+    var guests = JSON.parse(guestsJSON);
+
+    if (id < 0 || id >= guests.length || Number.isNaN(id)) {
+      return res.sendStatus(404);
+    }
+
+    res.set('Content-Type', 'text/plain');
+    res.send(guests[id]);
+  });
+});
+
+app.use(function (req, res) {
   res.sendStatus(404);
 });
 
