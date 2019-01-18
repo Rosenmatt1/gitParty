dd8d6f8126a929e0b1c7d2a8b94871c4802c2d15
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-01-17 17:21:50 -0700

    refactored to make 404 error handler

--- serverExpress.js
+++ serverExpress.js
@@ -4,9 +4,13 @@ var port = process.env.PORT || 8000;
 
 app.disable('x-powered-by');
 
-app.use(function (req, res) {
+app.get('/guests', (req, res) => {
   var guests = ['Mary', 'Don'];
   res.send(guests);
+});
+
+app.use((req, res) => {
+  res.sendStatus(404);
 });
 
 app.listen(port, function () {
